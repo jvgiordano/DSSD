bfa35bf662869013766b270efacad2c9a72575a6
jvgiordano <jgiordano100@gmail.com>
2018-12-17 14:37:09 +0100

    Modified startup.m for DSSD project folder Updated All_ERP_Within_Subject and All_Spectra_Within_Subject

--- eeg_analysis/All_ERP_Within_Subject.m
+++ eeg_analysis/All_ERP_Within_Subject.m
@@ -1,4 +1,10 @@
 %This script produces ERP Averages for all conditions and subjects
+%Setting 'k' below will allows one to select a certain subject
+%corresponding to that number, or a certain range of subjects ie k =1:5
+%will give ERP averages for all conditions of subjectts 1, 2, 3, 4, 5.
+%
+%Setting 'n' will do likewire but for conditions. With n=1 "Correct
+%Rejection", n=2 "False Alarm", n=3 "Hit", and n=4 "Miss"
 %
 %Made by: Jonathan Giordano
 %November 4, 2018
@@ -9,6 +15,7 @@
 con = ["cr", "fa", "hit", "miss"];
 condition = ["Correct Rejection", "False Alarm", "Hit", "Miss"];
 doc = " ";
+home = pwd;
 
 EEG.etc.eeglabvers = '14.1.2'; % this tracks which version of EEGLAB is being used, you may ignore it
 for k=1:19
@@ -22,10 +29,17 @@ for k=1:19
     for n=1:4
         doc = sprintf('%02d%s.set',k,con(n)); %sprintf must be used for newer Matlab versions, filename is of form '01cr.set'
         title = sprintf('ERP Data - Subject %02d - %s',k,condition(n)); %Create Title for plots
-        % For PC
-        % EEG = pop_loadset('filename',doc,'filepath','C:\\Users\\jonny\\Desktop\\Stage\\dssd_divided\\');
-        % For Mac
-        EEG = pop_loadset('filename',doc,'filepath','/Volumes/JVG_USB/Stage/dssd_divided/');
+        
+        % WINDOWS
+              
+        EEG = pop_loadset('filename',doc,'filepath', strcat(home, '\data\dssd_divided'));
+        
+        
+        % MAC
+%       
+%       EEG = pop_loadset('filename',doc,'filepath',strcat(home, '/data/dssd_divided'));
+%       
+         
         EEG = eeg_checkset( EEG );
         figure(); pop_timtopo(EEG, [-200  598], [NaN], title);
         
