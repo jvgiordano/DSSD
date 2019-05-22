8d340dd888ea78f2fb3a3c19e1089f23c4df884f
jvgiordano <jgiordano100@gmail.com>
2019-05-22 16:25:29 +0200

    Continued progress on Plot_Classification for easier plottig of analysis
    Discovered major bug in the recoding_triggers script. This has now been correct, but the classification must be run again and proper script titles updated

---
+++ decoding/Plot_Classification.m
@@ -0,0 +1,88 @@
+% SUMMARY: This script is to plot subject results, statistics,
+% averagaed plot, topographic map, and temporal generalization
+%
+% INPUT: Requires appropriate data from /Results Folder
+%
+% OUTPUT: Individual and Group Plots, Temporal Generalization, and Scalp
+% Maps
+%
+% USAGE:
+%
+% Made by: Jonathan Giordano
+% May 22, 2019
+
+%% Compute Statistics, plot all single subject results 
+
+
+cfg = [];
+cfg.startdir = 'C:\Users\jonny\Desktop\DSSD\Results\Flipped-(-0.2,-0.1)\';
+cfg.mpcompcor_method = 'cluster_based';
+%cfg.reduce_dims = 'diag'; %Train and test on the same time points
+cfg.cluster_pval = 0.05; %Sets P-Value
+%cfg.channelpool = 'ALL_NOSELECTION'; %Selection of electrodes from first analysis
+%cfg.splinefreq = 10; %Run a low pass filter at given frequency
+
+%cfg.singleplot = true;
+cfg.plot_order = { %'Correct_Response_AUC' ...
+                   'Stimulus_Movement_AUC' ...
+                   %'Perceived_Movement_AUC' 
+                   };
+
+mvpa_stats = adam_compute_group_MVPA(cfg);
+cfg.acclim = [0.48 0.52];
+adam_plot_MVPA(cfg, mvpa_stats);
+
+%%
+%Set Configuration settings
+figure(1)
+title('Stimulus Movement AUC')
+cfg = [];
+cfg.startdir = strcat(pwd + "\Resuts\Flipped\Stimulus_Movement_AUC");
+cfg.mpcompcor_method = 'cluster_based';
+cfg.reduce_dims = 'diag'; %Train and test on the same points
+%cfg.splinefreq = 11 %This is a low pass filter, remove it to see exact results
+cfg.plotsubjects = true;
+
+cfg.singleplot = true;
+%Calculate statistics, plot single subjects
+mvpa_stats = adam_compute_group_MVPA(cfg);
+cfg.acclim = [0.4 .6];
+adam_plot_MVPA(cfg, mvpa_stats);
+
+%% Plot combined results
+% Set Configuration Settings
+cfg = [];
+cfg.singleplot = true;
+cfg.plotsubjects = true;
+cfg.acclim = [0.4 .6];
+
+%Plot combined results
+adam_plot_MVPA(cfg, mvpa_stats);
+title({'Stimulus Movement', 'Parietal Electrodes'})
+
+%% Plot Topographic Maps
+
+%Configuration Settings
+cfg = [];
+cfg.mpcompcor_method = 'cluster_based';
+cfg.plotweights_or_pattern = 'covpatten';
+cfg.weightlim = [-0.6 0.6];
+cfg.timelim = [500 550];
+
+%Plot Topo map
+adam_plot_BDM_weights(cfg, mvpa_stats);
+
+%% Plot Temporal Generalization
+
+
+%Set Configuration Settings
+cfg = [];
+cfg.startdir = char(pwd + "\Resuts\Flipped\Stimulus_Movement_AUC");
+cfg.mpcompcor_method = 'cluster_based';
+cfg.iterations = 250;
+mvpa_stats = adam_compute_group_MVPA(cfg);
+
+%Plot Temporal Generalization
+
+cfg = [];
+adam_plot_MVPA(cfg, mvpa_stats);
